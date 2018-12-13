aace37a37fa78aca330ead875cbdaf25ef653b17
Samuel <sam101@hotmail.fr>
2018-12-13 16:33:43 +0200

    last commit of the day, need to check the error

--- src/app/src/main/java/com/example/betitarev/betitarev/activities/MainActivity.java
+++ src/app/src/main/java/com/example/betitarev/betitarev/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.betitarev.betitarev.activities;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,8 +9,14 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.betitarev.betitarev.R;
 import com.example.betitarev.betitarev.fragment.OpenedBetActivity;
@@ -16,7 +24,12 @@ import com.example.betitarev.betitarev.fragment.PlaceBetActivity;
 import com.example.betitarev.betitarev.fragment.ProfileActivity;
 import com.example.betitarev.betitarev.fragment.StatisticsActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    String[] arrays = new String[]{"98411", "98422", "98433", "98444", "98455"};
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_place_bet:
                     fragment = new PlaceBetActivity();
                     loadFragment(fragment);
+                    runPlaceBet();
                     return true;
                 case R.id.navigation_opened_bet:
                     fragment = new OpenedBetActivity();
@@ -58,6 +72,44 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
+
+    private void runPlaceBet() {
+        Toolbar tbMainSearch = (Toolbar) findViewById(R.id.toolbar);
+        ListView lvToolbarSearch = (ListView) findViewById(R.id.toolbarsearch);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrays);
+        Log.e("hello", lvToolbarSearch.toString());
+        lvToolbarSearch.setAdapter(adapter);
+        setSupportActionBar(tbMainSearch);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.search, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        MenuItem mSearchmenuItem = menu.findItem(R.id.menu_toolbarsearch);
+        SearchView searchView = (SearchView) mSearchmenuItem.getActionView();
+        searchView.setQueryHint("enter Text");
+        searchView.setOnQueryTextListener(this  );
+        Log.d(TAG, "onCreateOptionsMenu: mSearchmenuItem->" + mSearchmenuItem.getActionView());
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Log.d(TAG, "onQueryTextSubmit: query->"+query);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        Log.d(TAG, "onQueryTextChange: newText->" + newText);
+        adapter.getFilter().filter(newText);
+        return true;
+    }
 
     /**
      * loading fragment into FrameLayout
