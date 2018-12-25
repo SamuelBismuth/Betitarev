d2fbe2b4f4912690d2dba1a98620386b762fc06d
Yishay1 <31584757+Yishay1@users.noreply.github.com>
2018-12-25 15:26:26 +0200

    working on current admin
    functions:
    1. getInstance(BasicAdmin ba, String userId)
    2. static CurrentAdmin getInstance()

--- src/app/src/main/java/com/example/betitarev/betitarev/objects/CurrentAdmin.java
+++ src/app/src/main/java/com/example/betitarev/betitarev/objects/CurrentAdmin.java
@@ -1,11 +1,23 @@
 package com.example.betitarev.betitarev.objects;
 
-public class CurrentAdmin implements Admin {
+public class CurrentAdmin extends User{
 
-    private String userid; // path to the picture in our server.
 
-    static CurrentPlayer instance = null; // Singleton implementation.
-    
+    static CurrentAdmin instance = null; // Singleton implementation.
+
+    static public CurrentAdmin getInstance(BasicAdmin ba, String userId) {
+        if(instance != null)
+            return instance;
+        instance = new CurrentAdmin(ba, userId);
+        return instance;
+    }
+    private CurrentAdmin(BasicAdmin ba, String userId) {
+        super(ba, userId);
+    }
+
+    public static CurrentAdmin getInstance() {
+        return instance;
+    }
 
 
 }
