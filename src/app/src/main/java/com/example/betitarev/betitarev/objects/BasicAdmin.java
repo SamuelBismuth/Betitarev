01969d10c825d513788db5b812e8d23663edf4e7
Yishay1 <31584757+Yishay1@users.noreply.github.com>
2018-12-25 14:28:04 +0200

    start working on basic admin

--- src/app/src/main/java/com/example/betitarev/betitarev/objects/BasicAdmin.java
+++ src/app/src/main/java/com/example/betitarev/betitarev/objects/BasicAdmin.java
@@ -1,5 +1,8 @@
 package com.example.betitarev.betitarev.objects;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class BasicAdmin extends User implements Admin {
 
     public BasicAdmin(String name, String familyName, Mail mail, String pushToken) {
@@ -8,11 +11,24 @@ public class BasicAdmin extends User implements Admin {
 
     @Override
     public boolean sendWarning(User player) {
+        DatabaseReference mFirebaseDatabase;
+        FirebaseDatabase mFirebaseInstance;
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference("notifcations");
+        String notifId = mFirebaseDatabase.push().getKey();
+        String message = "";
+        String senderToken = "";
+        String receiverToken = "";
+        Notification notif = new Notification("Bet request!!", message, senderToken, receiverToken);
+        mFirebaseDatabase.child(notifId).setValue(notif);
+
         return false;
     }
 
     @Override
     public boolean removePlayer(User player) {
+
+
         return false;
     }
 
