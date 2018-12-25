4316bb13136778f605d943e09aff33dbe02ed54a
Yishay1 <31584757+Yishay1@users.noreply.github.com>
2018-12-25 15:58:33 +0200

    function removePlayer(User player) in BasicAdmin

--- src/app/src/main/java/com/example/betitarev/betitarev/objects/BasicAdmin.java
+++ src/app/src/main/java/com/example/betitarev/betitarev/objects/BasicAdmin.java
@@ -3,10 +3,16 @@ package com.example.betitarev.betitarev.objects;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class BasicAdmin extends User implements Admin {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BasicAdmin extends User implements Admin {
+    static List<String> AdminPushToken;
     public BasicAdmin(String name, String familyName, Mail mail, String pushToken) {
         super(name, familyName, mail, pushToken);
+        if(AdminPushToken==null)
+            AdminPushToken= new ArrayList<>();
+        AdminPushToken.add(pushToken);
     }
 
     @Override
@@ -19,14 +25,17 @@ public class BasicAdmin extends User implements Admin {
         String message = "this is warning before expel.\n please avoid of cheating and\\or unappropriate bets";
         Notification notif = new Notification("warning before expel:", message, CurrentAdmin.getInstance().getPushToken(), player.getPushToken());
         mFirebaseDatabase.child(notifId).setValue(notif);
-
         return false;
     }
 
     @Override
     public boolean removePlayer(User player) {
-
-
+        DatabaseReference mFirebaseDatabase;
+        FirebaseDatabase mFirebaseInstance;
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference();
+        if(!AdminPushToken.contains(player.getPushToken()))
+            mFirebaseDatabase.child(player.getPushToken()).removeValue();
         return false;
     }
 
