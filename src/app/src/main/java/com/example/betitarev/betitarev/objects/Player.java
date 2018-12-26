4152a3b7563cc28c995a6c415c1e3a318b98106e
Samuel <sam101@hotmail.fr>
2018-12-26 13:42:24 +0200

    fix

--- src/app/src/main/java/com/example/betitarev/betitarev/objects/Player.java
+++ src/app/src/main/java/com/example/betitarev/betitarev/objects/Player.java
@@ -2,18 +2,14 @@ package com.example.betitarev.betitarev.objects;
 
 public class Player extends User {
 
-    public Player(){}
-
-    public Player(String name, String familyName, Mail mail, String pushToken) {
-        super(name, familyName, mail, pushToken);
+    public Player() {
     }
 
     public Player(Player player) {
         super(player);
     }
 
-
     public Player(String name, String familyName, Mail email, String userId, String pushToken) {
-        super(name,familyName,email,userId,pushToken);
+        super(name, familyName, email, userId, pushToken);
     }
 }
