fb1769b288de117b826a83a1a9fb7fed2f043253
Yishay1 <31584757+Yishay1@users.noreply.github.com>
2018-12-27 20:02:24 +0200

    Update Adapter.java
    almost done. only need to design

--- src/app/src/main/java/com/example/betitarev/betitarev/fragment/Adapter.java
+++ src/app/src/main/java/com/example/betitarev/betitarev/fragment/Adapter.java
@@ -1,5 +1,6 @@
 package com.example.betitarev.betitarev.fragment;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     private List<Bet> mDataset;
+    String[] myDataset={"sksksk","sjsjsj","nnnnnnnn"};
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -27,7 +29,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
     // Provide a suitable constructor (depends on the kind of dataset)
     public Adapter(List<Bet> myDataset) {
-        mDataset = myDataset;
+        mDataset=myDataset;
     }
 
     // Create new views (invoked by the layout manager)
@@ -35,7 +37,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     public Adapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
         TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.activity_opened_bet, parent, false);
+                .inflate(R.layout.my_text_view, parent, false);
 
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
@@ -46,16 +48,20 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mTextView.setText("bettor 1: "+mDataset.get(position).getPlayer1()+
-                ", bettor 2: "+mDataset.get(position).getPlayer2()+
-                "\npharse: "+mDataset.get(position).getPhrase()+
-                ", money: "+mDataset.get(position).getFictiveMoney());
+        Bet b=mDataset.get(position);
+
+        holder.mTextView.setText("bettor 1: "+b.getPlayer1().getUser().getName()+", guess: "+b.getPlayer1().getGuessing()+
+                "\nbettor 2: "+b.getPlayer2().getUser().getName()+", guess: "+b.getPlayer2().getGuessing()+
+                "\npharse: "+b.getPhrase()+
+                "\nmoney: "+Integer.toString(b.getFictiveMoney().getAmount()));
+        holder.mTextView.setTextColor(Color.BLACK);
+        //holder.mTextView.
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.size();
+        return myDataset.length;
     }
 }
 
