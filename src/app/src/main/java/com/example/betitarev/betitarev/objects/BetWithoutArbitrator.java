376271ab157d5e205f0693f617d962d4ba81cf93
Samuel <sam101@hotmail.fr>
2018-12-27 16:49:31 +0200

    attention, commit with bugs but need to fix it

--- src/app/src/main/java/com/example/betitarev/betitarev/objects/BetWithoutArbitrator.java
+++ src/app/src/main/java/com/example/betitarev/betitarev/objects/BetWithoutArbitrator.java
@@ -2,8 +2,15 @@ package com.example.betitarev.betitarev.objects;
 
 public class BetWithoutArbitrator extends Bet {
 
+    Arbitrator arbitrator;
+
     public BetWithoutArbitrator(Bettor player1, Bettor player2, String phrase, FictiveMoney fictiveMoney) {
-        super(player1, player2, phrase, fictiveMoney, new Arbitrator(player1.getUser()));
+        super(player1, player2, phrase, fictiveMoney,  new Arbitrator(player1.getUser()));
+        this.arbitrator = new Arbitrator(player1.getUser());
     }
 
+    @Override
+    public Arbitrator getArbitrator() {
+        return arbitrator;
+    }
 }
