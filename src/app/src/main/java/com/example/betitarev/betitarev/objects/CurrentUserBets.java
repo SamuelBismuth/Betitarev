0b3b67aabb23a0ce8db5ed985ee986d5bcc104e7
Yishay1 <31584757+Yishay1@users.noreply.github.com>
2018-12-27 16:33:43 +0200

    Update CurrentUserBets.java

--- src/app/src/main/java/com/example/betitarev/betitarev/objects/CurrentUserBets.java
+++ src/app/src/main/java/com/example/betitarev/betitarev/objects/CurrentUserBets.java
@@ -2,9 +2,9 @@ package com.example.betitarev.betitarev.objects;
 
 import java.util.List;
 
-public class CurrentUserBets {
+public class CurrentUserBets  {
     private static CurrentUserBets instance = null; // Singleton implementation.
-    private List<Bet> Bets;
+    private List<Bet> bets;
 
     static public CurrentUserBets getInstance(List<Bet> Bets) {
         if(instance != null)
@@ -14,13 +14,15 @@ public class CurrentUserBets {
     }
 
     private CurrentUserBets(List<Bet> Bets) {
-        this.Bets = Bets;
+        this.bets = Bets;
     }
-
+    public List<Bet> getBets(){
+        return bets;
+    }
     public static CurrentUserBets getInstance() {
         return instance;
     }
     public void addBet(Bet bet){
-        Bets.add(bet);
+        bets.add(bet);
     }
 }
